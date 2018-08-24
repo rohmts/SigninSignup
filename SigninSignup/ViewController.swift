340cdf6f2f9f7c1828642beae3fcbcfb745dc7d9
Rohmats <rohmatsuseno@gmail.com>
2018-08-24 10:22:06 +0700

    initial commit

---
+++ SigninSignup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SigninSignup
+//
+//  Created by Rohmts on 24/08/18.
+//  Copyright © 2018 Innovation Bdg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
