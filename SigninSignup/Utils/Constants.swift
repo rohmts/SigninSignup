c48c2bc95906ea1c47992e23e3b07e807f1b3add
Rohmats <rohmatsuseno@gmail.com>
2018-08-24 16:23:58 +0700

    add textField & Button

---
+++ SigninSignup/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SigninSignup
+//
+//  Created by Rohmat Suseno on 24/08/18.
+//  Copyright © 2018 Innovation Bdg. All rights reserved.
+//
+
+import UIKit
+
+let GREEN_THEME = UIColor(displayP3Red: 109/255, green: 201/255, blue: 149/255, alpha: 1)
--- SigninSignup/ViewController.swift
+++ SigninSignup/ViewController.swift
@@ -10,9 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emailTextField: UITextField = {
+        let tv = UITextField()
+        tv.placeholder = "Email"
+        tv.textColor = .white
+        tv.backgroundColor = .red
+        return tv
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tv = UITextField()
+        tv.placeholder = "Password"
+        tv.textColor = .white
+        tv.backgroundColor = .blue
+        return tv
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = .purple
+        return button
+    }()
+    
+    let haveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        let attributedTitle = NSAttributedString(string: "Don't have an account?", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = GREEN_THEME
+        
+        setupTextFieldComponents()
+        setupLoginButton()
+        setupHaveAccount()
+    }
+    
+    fileprivate func setupTextFieldComponents() {
+        setupEmailField()
+        setupPasswordField()
+    }
+    
+    fileprivate func setupEmailField() {
+        view.addSubview(emailTextField)
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    fileprivate func setupPasswordField() {
+        view.addSubview(passwordTextField)
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 8).isActive = true
+        passwordTextField.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor, constant: 0).isActive = true
+        passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor, constant: 0).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    fileprivate func setupLoginButton() {
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 8).isActive = true
+        loginButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor, constant: 0).isActive = true
+        loginButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor, constant: 0).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    fileprivate func setupHaveAccount() {
+        view.addSubview(haveAccountButton)
+        haveAccountButton.translatesAutoresizingMaskIntoConstraints = false
+        haveAccountButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
+        haveAccountButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
+        haveAccountButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
+        haveAccountButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 
 }
